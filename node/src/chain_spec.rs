75983872ccca90cb4e42514a4b4fd932c0d01109
Kaicho Sun <kaichaosuna@gmail.com>
2022-04-01 16:42:25 +0800

    Steps to run public network.

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -309,6 +309,7 @@ fn testnet_genesis(
 			invulnerables: initial_authorities.iter().map(|x| x.0.clone()).collect(),
 			slash_reward_fraction: Perbill::from_percent(10),
 			stakers,
+			// TODO: ForceEra::ForceNone
 			..Default::default()
 		},
 		babe: BabeConfig { authorities: vec![], epoch_config: Some(BABE_GENESIS_EPOCH_CONFIG) },
